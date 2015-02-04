dc7db1757ead8e1c48c9ad0e5eae735ca7eaa76e
Hayden Shelton <hayden.shelton@utah.edu>
2015-02-03 19:30:55 -0700

    update

--- BoggleWebServer/BoggleWebServer/ServerRoutes.cs
+++ BoggleWebServer/BoggleWebServer/ServerRoutes.cs
@@ -8,7 +8,7 @@ namespace BoggleWebServer
 {
     class ServerRoutes
     {
-        private const string baseIP = "localhost";
+        private const string baseIP = "http://localhost";
         private const string portNum = ":3012/";
         public const string INDEX = baseIP+portNum;
         public const string UPDATE = baseIP + portNum + "Update/";
